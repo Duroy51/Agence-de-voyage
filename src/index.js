d722cebcba2100f168d141aad8498e43975801cd
erickoghene@gmail.com <erickoghene@gmail.com>
2024-11-02 09:34:18 +0100

    some changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {AppRoute} from "./Router/Router";
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <div className='min-h-screen overflow-y-auto overflow-x-hidden'>
+                <AppRoute />
+            </div>
+        </BrowserRouter>
+
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
